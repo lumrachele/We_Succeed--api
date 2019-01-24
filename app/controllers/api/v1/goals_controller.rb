e6d1595dc9e0a5eaf713d04e2142088a2f167544
satyasinha94 <satyasinha94@gmail.com>
2019-01-24 12:02:53 -0500

    create for goal controller

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::GoalsController < ApplicationController
 
 
+  def create
+    @goal = Goal.create(goal_params)
+    render json: @goal, status: :ok
+  end
+
+
   def show
     @goal = Goal.find(params[:id])
     render json: @goal, status: :ok
